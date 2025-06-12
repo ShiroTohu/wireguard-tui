5e4dd94790b23573d20be3b4238f43540a77078d
shirotohu <44159210+ShiroTohu@users.noreply.github.com>
2025-06-12 14:10:54 +1000

    Moved Widgets to main.py and made background transparent
    I'll move the widgets to separate modules once the project starts going.
    Currently I will just leave them as is. I also made the background
    transparent. This should address the concern with the background...
    hopefully... I haven't checked.

--- source/main.py
+++ source/main.py
@@ -1,12 +1,32 @@
-from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, ListView, ListItem, Label, Static
-from textual.containers import Container
+from textual.app import App, ComposeResult, Widget
+from textual.widgets import ListView, Label, Static
+
+from rich import box
 from rich.panel import Panel
 
 
 class TunnelsSelect(Static):
     def render(self) -> Panel:
-        return Panel("Stuff", title="Tunnels", border_style="white")
+        return Panel("Stuff", title="Tunnels", border_style="red",
+                     box=box.SQUARE)
+
+
+class Logs(Widget):
+    def render(self) -> Panel:
+        return Panel("Stuff", title="Tunnels", border_style="white",
+                     box=box.SQUARE)
+
+
+class TunnelInformation(Widget):
+    def render(self) -> Panel:
+        return Panel("Information about current configuration",
+                     title="Tunnels", border_style="white", box=box.SQUARE)
+
+
+class NetworkInformation(Widget):
+    def render(self) -> Panel:
+        return Panel("Information about current configuration",
+                     title="Tunnels", border_style="white", box=box.SQUARE)
 
 
 class WireguardApp(App):
@@ -14,10 +34,13 @@ class WireguardApp(App):
 
     def compose(self) -> ComposeResult:
         yield TunnelsSelect()
+        yield Logs()
+        yield NetworkInformation()
+        yield TunnelInformation()
 
     def on_mount(self) -> None:
         self.title = "Wireguard TUI"
-        self.screen.styles.background = "black"
+        self.screen.styles.background = "transparent"
 
     def on_list_view_selected(self, event: ListView.Selected) -> None:
         selected = event.item.query_one(Label).text
--- source/widgets/tunnels_list.py
+++
@@ -1,6 +0,0 @@
-from rich.panel import Panel
-
-
-class TunnelList(Widget):
-    def __init__(self):
-        super().__init__()
