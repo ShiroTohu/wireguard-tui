b3193fc33e9e61533a35bdaa32660cf9881bc194
shirotohu <44159210+ShiroTohu@users.noreply.github.com>
2025-06-12 21:04:48 +1000

    Added FocusWidget
    This is so that the widgets can be switched by pressing the tab key.

--- source/main.py
+++ source/main.py
@@ -1,50 +1,72 @@
 from textual.app import App, ComposeResult, Widget
-from textual.widgets import ListView, Label, Static
+from textual.widgets import ListView, Label, Footer
+from textual import events
 
 from rich import box
 from rich.panel import Panel
 
 
-class TunnelsSelect(Static):
+class FocusPanel(Widget):
+    def render(self) -> Panel:
+        return Panel("Stuff", title="Tunnels", border_style="red",
+                     box=box.SQUARE)
+
+    def on_focus(self) -> None:
+        return None
+
+
+class TunnelsSelect(FocusPanel):
     def render(self) -> Panel:
         return Panel("Stuff", title="Tunnels", border_style="red",
                      box=box.SQUARE)
 
 
-class Logs(Widget):
+class Logs(FocusPanel):
     def render(self) -> Panel:
         return Panel("Stuff", title="Tunnels", border_style="white",
                      box=box.SQUARE)
 
 
-class TunnelInformation(Widget):
+class TunnelInformation(FocusPanel):
     def render(self) -> Panel:
         return Panel("Information about current configuration",
                      title="Tunnels", border_style="white", box=box.SQUARE)
 
 
-class NetworkInformation(Widget):
+class NetworkInformation(FocusPanel):
     def render(self) -> Panel:
         return Panel("Information about current configuration",
                      title="Tunnels", border_style="white", box=box.SQUARE)
 
 
 class WireguardApp(App):
-    BINDINGS = [("q", "quit", "quit")]
+    BINDINGS = [
+        ("q", "quit", "quit"),
+        ("tab", "next_panel", "Next Panel (Tab)")
+    ]
 
     def compose(self) -> ComposeResult:
         yield TunnelsSelect()
         yield Logs()
         yield NetworkInformation()
         yield TunnelInformation()
+        yield Footer()
 
     def on_mount(self) -> None:
         self.title = "Wireguard TUI"
         self.screen.styles.background = "transparent"
+        self.focus_index = 0
 
     def on_list_view_selected(self, event: ListView.Selected) -> None:
         selected = event.item.query_one(Label).text
         self.console.print(f"Selected: {selected}")
+
+    def on_key(self, event: events.Key):
+        if event.key == "tab":
+            self.focus_index = self.focus_index + 1
+
+    def action_next_panel():
+        pass
 
 
 if __name__ == "__main__":
