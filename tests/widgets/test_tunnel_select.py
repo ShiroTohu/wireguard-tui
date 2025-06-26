460bb22d2374660cd78ae08b6c5ed7d7c4d72e68
shirotohu <44159210+ShiroTohu@users.noreply.github.com>
2025-06-26 13:23:33 +1000

    Wrote first navigation test
    This test confirms that the selected item in the widget is what is
    expected. I think this will need some expanding upon but at the moment
    it is pretty good!

--- tests/widgets/test_tunnel_select.py
+++ tests/widgets/test_tunnel_select.py
@@ -1,1 +1,16 @@
 import pytest
+
+from wireguard_tui.app import WireGuardApp
+from wireguard_tui.widgets import TunnelSelect
+
+
+@pytest.mark.asyncio
+async def test_navigation():
+    "Test application navigation"
+    app = WireGuardApp()
+    async with app.run_test() as pilot:
+        await pilot.press("j")  # Down
+        assert app.query_one(TunnelSelect).select_index == 1
+
+        await pilot.press("k")  # Up
+        assert app.query_one(TunnelSelect).select_index == 0
