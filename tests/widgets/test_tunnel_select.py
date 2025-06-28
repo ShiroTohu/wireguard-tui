d235ee6ed953cea26cdeaf8ffb8d3689e82a8be6
shirotohu <44159210+ShiroTohu@users.noreply.github.com>
2025-06-28 14:47:12 +1000

    Created app for tunnel select test
    Created app for the select widget, that way the test can raise an error
    if it cannot contact the daemon instead of returning a placeholder list.
    
    TLDR: Made app for tests which allows now allows for other features to
    be implemented.

--- tests/widgets/test_tunnel_select.py
+++ tests/widgets/test_tunnel_select.py
@@ -1,13 +1,30 @@
 import pytest
 
+from textual.app import App, ComposeResult
+
 from wireguard_tui.app import WireGuardApp
 from wireguard_tui.widgets import TunnelSelect
+
+
+class SelectApp(App):
+    """Test Application to store widget"""
+
+    def compose(self) -> ComposeResult:
+        yield TunnelSelect(["Option 1", "Option 2", "Option 3"])
+
+    def key_j(self) -> None:
+        # self.query_one(RichLog).write("move_down")
+        self.query_one(TunnelSelect).move_down()
+
+    def key_k(self) -> None:
+        # self.query_one(RichLog).write("move_up")
+        self.query_one(TunnelSelect).move_up()
 
 
 @pytest.mark.asyncio
 async def test_navigation():
     "Test application navigation"
-    app = WireGuardApp()
+    app = SelectApp()
     async with app.run_test() as pilot:
         await pilot.press("j")  # Down
         assert app.query_one(TunnelSelect).select_index == 1
